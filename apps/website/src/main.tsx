fb6dac8dc942c10837c7672ad1c3f7b8de7d8927
Tran Van Co <erpcloud@MacBook-Air-cua-Tran-2.local>
2026-08-09 12:59:02 +0700

    feat(website): connect marketing site to Lio docs

--- apps/website/src/main.tsx
+++ apps/website/src/main.tsx
@@ -4,5 +4,6 @@ import App from "./App"
 import "./styles.css"
 import "./chrome-overrides.css"
 import "./fab.css"
+import "./docs-launch.css"
 
 createRoot(document.getElementById("root")!).render(<StrictMode><App /></StrictMode>)
